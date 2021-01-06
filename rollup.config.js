3cb6701901799f0409e3f62ef9095f6818674702
Andy Costello <andrew.costello@metrisk.com>
2021-01-06 12:38:55 +0000

    Provide dedicated cjs build

--- rollup.config.js
+++ rollup.config.js
@@ -3,10 +3,17 @@ import typescript from 'rollup-plugin-typescript2'
 
 export default {
   input: 'src/index.ts',
-  output: {
-    file: 'dist/index.js',
-    exports: 'auto',
-  },
+  output: [
+    {
+      file: 'dist/index.js',
+      format: 'es',
+    },
+    {
+      file: 'dist/index.cjs.js',
+      format: 'cjs',
+      exports: 'auto',
+    }
+  ],
   plugins: [
     nodeResolve({ extensions: ['.js','.ts'] }),
     typescript(),
